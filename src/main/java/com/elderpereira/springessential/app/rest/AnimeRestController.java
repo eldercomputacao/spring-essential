155fc22cfd78297e4a702c864613b09290adf124
Elder Pereira <elder.computacao@gmail.com>
2023-12-20 16:52:06 -0300

    feat: added pageable

--- src/main/java/com/elderpereira/springessential/app/rest/AnimeRestController.java
+++ src/main/java/com/elderpereira/springessential/app/rest/AnimeRestController.java
@@ -11,6 +11,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -96,4 +99,30 @@ public interface AnimeRestController {
             @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
     })
     ResponseEntity<AnimeWithPostsResponse> animeWithPosts(@PathVariable long id);
+
+    @GetMapping(path = "pageable-with-pagemodel", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
+            MediaTypeYmal.APPLICATION_YAML_VALUE})
+    @Operation(summary = "Finds all 'animes'", responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema =
+                    @Schema(implementation = AnimeResponse.class)))
+            }),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+    })
+    ResponseEntity<PagedModel<EntityModel<AnimeResponse>>> listWithPagedModel(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                                              @RequestParam(value = "size", defaultValue = "12") Integer size,
+                                                                              @RequestParam(value = "direction", defaultValue = "asc") String direction);
+    @GetMapping(path = "pageable-with-page", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
+            MediaTypeYmal.APPLICATION_YAML_VALUE})
+    @Operation(summary = "Finds all 'animes'", responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema =
+                    @Schema(implementation = AnimeResponse.class)))
+            }),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+    })
+    ResponseEntity<Page<AnimeResponse>> listWithPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                     @RequestParam(value = "size", defaultValue = "12") Integer size,
+                                                     @RequestParam(value = "direction", defaultValue = "asc") String direction);
+
 }
